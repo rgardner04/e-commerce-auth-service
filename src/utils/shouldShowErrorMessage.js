0d36687605a5e548dacb87a1f61947c50f323020
Ryan Gardner <gardnerryan2004@gmail.com>
2026-05-16 22:44:33 -0400

    fix: logic in shouldShowErrorMessage & errorTypeEnum INVALID_ROLE spelling

--- src/utils/shouldShowErrorMessage.js
+++ src/utils/shouldShowErrorMessage.js
@@ -1,16 +1,15 @@
 const errorTypeEnum = require("../enums/errorTypeEnum");
 
 function shouldShowErrorMessage(error) {
-  let showErrorMessage = false;
   switch (error.type) {
     case errorTypeEnum.INVALID_CREDENTIALS:
     case errorTypeEnum.USER_NOT_FOUND:
-      showErrorMessage = false;
-      break;
+      return false;
     case errorTypeEnum.DEFAULT_ERROR:
-    case errorTypeEnum.INVALIDE_ROLE:
-      showErrorMessage = true;
-      break;
+    case errorTypeEnum.INVALID_ROLE:
+      return true;
+    default:
+      return false;
   }
 }
 
